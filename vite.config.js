24abe1231575051f54d1bfa8b6eff83db94452ac
alperendemirtas <alperendemirtas90@gmail.com>
2025-03-22 02:42:32 +0300

    deneme

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  // diğer yapılandırmalar...
-}); 
+  base: 'http://eng4all.tr/',
+  // ... other config settings
+}) 
